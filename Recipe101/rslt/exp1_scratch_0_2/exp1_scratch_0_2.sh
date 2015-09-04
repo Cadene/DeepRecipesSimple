82a381ceac865b734856d6511813611f7202151f
Tamazy <tamazy@laposte.net>
2015-09-04 07:07:37 +0200

    sync

---
+++ Recipe101/rslt/exp1_scratch_0_2/exp1_scratch_0_2.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 th -i main.lua -path2save ./rslt/exp1_scratch_0_2 -lr 6e-1 -lrd 8e-5 -wd 1e-3 -m 0.6 -lrf_conv 1 -pretrain 0
---
+++ Recipe101/rslt/exp1_scratch_1_2/exp1_scratch_1_2.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=1 th -i main.lua -path2save ./rslt/exp1_scratch_1_2 -lr 4e-1 -lrd 8e-5 -wd 1e-3 -m 0.6 -lrf_conv 1 -pretrain 0
