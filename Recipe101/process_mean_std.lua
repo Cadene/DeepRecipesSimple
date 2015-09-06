00ab9a0b00f043e13a5c11cbe475a9dd2d307c23
Tamazy <tamazy@laposte.net>
2015-09-06 17:46:34 +0200

    fix process_mean_std
    false ponderation of classes

--- Recipe101/process_mean_std.lua
+++ Recipe101/process_mean_std.lua
@@ -4,8 +4,7 @@ torch.setnumthreads(4)
 
 timer = torch.Timer()
 
-
-
+load_mean = true
 path2train = '/home/cadene/data/recipe_101_augmented/train/'
 
 mean = torch.zeros(3, 221, 221)
@@ -21,11 +20,11 @@ local is_in = function (string, path2esc)
     return false
 end
 
+nb_img = 0
 timer_mean = torch.Timer()
 for _, class in pairs(paths.dir(path2train)) do
     if not is_in(class, path2esc) then -- rm path2esc values
         local mean_class = torch.zeros(3, 221, 221)
-        local nb_img = 0
         for _, path_img in pairs(paths.dir(path2train..class)) do
             if not is_in(path_img, path2esc) then
                 local path2img = paths.concat(path2train, class, path_img)
@@ -41,23 +40,25 @@ for _, class in pairs(paths.dir(path2train)) do
                 nb_img = nb_img + 1
             end
         end
-        mean_class = mean_class / nb_img
         mean:add(mean_class)
         s = timer:time().real
-        print('Mean of '..class..' done in '
-            ..string.format("%.2d:%.2d:%.2d", s/(60*60), s/60%60, s%60)) 
+        print(class..' done in '
+            ..string.format("%.2d:%.2d:%.2d", s/(60*60), s/60%60, s%60))
+        print('mean value = '..mean[{1,1,1}]..' / '..nb_img) 
         timer:reset()
     end
 end
-mean = mean / 101
+mean = mean / nb_img
 image.save('mean_augmented.jpg', mean)
 s = timer_mean:time().real
 print('Mean done in '
-    ..string.format("%.2d:%.2d:%.2d", s/(60*60), s/60%60, s%60)) 
+    ..string.format("%.2d:%.2d:%.2d", s/(60*60), s/60%60, s%60))
 
+nb_img = 0
 timer_std = torch.Timer()
 for _, class in pairs(paths.dir(path2train)) do
     if not is_in(class, path2esc) then -- rm path2esc values
+        local std_class = torch.zeros(3, 221, 221)
         for _, path_img in pairs(paths.dir(path2train..class)) do
             if not is_in(path_img, path2esc) then
                 local path2img = paths.concat(path2train, class, path_img)
@@ -71,15 +72,19 @@ for _, class in pairs(paths.dir(path2train)) do
                 end
                 local tmp = img - mean
                 tmp:pow(2)
-                std:add(tmp)
+                std_class:add(tmp)
+                nb_img = nb_img + 1
             end
         end
+        std:add(std_class)
         s = timer:time().real
-        print('Std of '..class..' done in '
+        print(class..' done in '
             ..string.format("%.2d:%.2d:%.2d", s/(60*60), s/60%60, s%60)) 
+        print('std value = sqrt ('..std[{1,1,1}]..' / '..nb_img..' )')
         timer:reset()
     end
 end
+std = std / nb_img
 std = std:sqrt()
 s = timer_std:time().real
 print('Std done in '
